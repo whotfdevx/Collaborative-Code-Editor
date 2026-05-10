41c564ceef9c6f450f543c30f7a943b8b7fa1c7c
whotfdevx <devananth2512@gmail.com>
2026-05-10 16:01:26 +0530

    Week 1: A11 setup complete - React + Node.js installed

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*', methods: ['GET', 'POST'] }
+});
+
+app.get('/', (req, res) => res.send('A11 Server running'));
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(5000, () => console.log('Server running on port 5000'));
